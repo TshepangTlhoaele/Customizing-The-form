172eb36d9ec92934c8a54758f09a7af3a22325c0
Lenovo-User <Lenovo-User@DESKTOP-KGOC38Q>
2024-01-19 08:54:10 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnEvansFundra.Click
+        MsgBox("This is Tshepang",, "Greeting")
+
+    End Sub
+End Class
